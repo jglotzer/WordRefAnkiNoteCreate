85f1a57cb8cab0374cdb8252c8c018e86d7a615f
John Glotzer <jglotzer@gmail.com>
2025-10-30 17:01:12 +0100

    Use Piper Web Server for genFrench.sh

--- genFrench.sh
+++ genFrench.sh
@@ -5,18 +5,34 @@ if [[ $# -ne 2 ]]; then
   exit 1
 fi
 
+TEXT="$1"
 FFMPEG="/usr/bin/ffmpeg"
 MPV="/usr/bin/mpv"
 MODEL="$HOME/.local/share/piper/voices/fr_FR-siwis-medium"
-TEXT="$1"
 OUTFILE_WAV="$(mktemp --suffix=.wav)"
 OUTFILE_MP3="$2.mp3"
-if ! python3 -m piper --model "$MODEL" --output-file "$OUTFILE_WAV" -- $TEXT
+PGREP="/usr/bin/pgrep"
+CURL="/usr/bin/curl"
+JQ="/usr/bin/jq"
+PIPER_SERVER="piper.http_server"
+
+if ! "${PGREP}" -f "${PIPER_SERVER}" > /dev/null
 then
-  echo "Piper generation for $TEXT failed, exiting."
+   echo "Piper HTTP server not running, starting in background."
+   python3 -m "${PIPER_SERVER}" --model "${MODEL}" &
+   sleep 3
+fi
+
+# https://stackoverflow.com/questions/48470049/build-a-json-string-with-bash-variables
+JSON_STRING=$($JQ -n --arg text "$TEXT" '{text: $text}')
+
+if ! $CURL -s -X POST -H 'Content-Type: application/json' -d "$JSON_STRING" -o "${OUTFILE_WAV}" localhost:5000
+then
+  echo "Curl to local server call failed."
   exit 1
 fi
-if ! $FFMPEG -i $OUTFILE_WAV -loglevel warning -y -vn -ar 44100 -ac 2 -b:a 192k $OUTFILE_MP3
+
+if ! $FFMPEG -i $OUTFILE_WAV -loglevel quiet -y -vn -ar 44100 -ac 2 -b:a 192k $OUTFILE_MP3
 then
   echo "MP3 generation with ffmpeg failed for $TEXT, exiting."
   rm -f $OUTFILE_WAV
