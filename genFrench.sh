7eb8b8695787f071279fe51e93f3b9fa90985cd4
John Glotzer <jglotzer@gmail.com>
2025-11-08 09:01:47 +0100

    add some shellcheck disable, simplify formatting

--- genFrench.sh
+++ genFrench.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
+# shellcheck disable=SC2086
 
 if [[ $# -ne 2 ]]; then
   echo "Usage: genFrench.sh text <outfile>"
@@ -16,17 +17,18 @@ CURL="/usr/bin/curl"
 JQ="/usr/bin/jq"
 PIPER_SERVER="piper.http_server"
 
-if ! "${PGREP}" -f "${PIPER_SERVER}" > /dev/null
+if ! $PGREP -f $PIPER_SERVER > /dev/null
 then
    echo "Piper HTTP server not running, starting in background."
-   python3 -m "${PIPER_SERVER}" --model "${MODEL}" &
+   python3 -m $PIPER_SERVER --model $MODEL &
    sleep 3
 fi
 
 # https://stackoverflow.com/questions/48470049/build-a-json-string-with-bash-variables
+# shellcheck disable=SC2016
 JSON_STRING=$($JQ -n --arg text "$TEXT" '{text: $text}')
-echo $JSON_STRING > /tmp/json_string
-if ! $CURL -s -X POST -H 'Content-Type: application/json' -d "$JSON_STRING" -o "${OUTFILE_WAV}" localhost:5000
+echo "$JSON_STRING" > /tmp/json_string
+if ! $CURL -s -X POST -H 'Content-Type: application/json' -d "$JSON_STRING" -o $OUTFILE_WAV localhost:5000
 then
   echo "Curl to Piper Webserver call failed."
   exit 1
@@ -34,7 +36,7 @@ fi
 
 if ! $FFMPEG -i $OUTFILE_WAV -loglevel quiet -y -vn -ar 44100 -ac 2 -b:a 192k $OUTFILE_MP3
 then
-  echo "MP3 generation with ffmpeg failed for $TEXT, exiting."
+  echo MP3 generation with ffmpeg failed for "$TEXT", exiting.
   rm -f $OUTFILE_WAV
   exit 1
 fi
