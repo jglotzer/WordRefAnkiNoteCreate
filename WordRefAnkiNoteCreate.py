9cc9cca1a047c2ddca77253183f64caac3155726
John Glotzer <jglotzer@gmail.com>
2025-11-11 18:17:35 +0100

    clean filenames better in WordRefAnkiNoteCreate.

--- WordRefAnkiNoteCreate.py
+++ WordRefAnkiNoteCreate.py
@@ -289,7 +289,14 @@ def main():
         # Add terminal space to avoid dropping final syllable.
         word_for_voice_lookup = f"{article}{word} "
         word_for_filename_base = f"{word}"
-        filename_base = word_for_filename_base.replace(" ", "_")
+        # To create a clean filename need to do a few things.
+        # Replace space with underscore.
+        # Remove characters that will confuse the shell.
+        before = " '"
+        after = "__"
+        remove = "?!"
+        translation_table = str.maketrans(before, after, remove)
+        filename_base = word_for_filename_base.translate(translation_table)
         output_mp3 = f"/tmp/{filename_base}.mp3"
         if se:
             if word_for_voice_lookup[0] in ("a", "e", "é", "i", "o", "ô", "u", "y"):
